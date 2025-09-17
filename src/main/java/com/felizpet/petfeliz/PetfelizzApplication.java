316aa72829e8113a31fad8dc2a286c009b446ac2
DevGermano <germanodev00@gmail.com>
2025-09-17 18:22:06 -0300

    Initial commit - Projeto Petfelizz Spring Boot

---
+++ src/main/java/com/felizpet/petfeliz/PetfelizzApplication.java
@@ -0,0 +1,13 @@
+package com.felizpet.petfeliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetfelizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetfelizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felizpet/Petfelizz/PetfelizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felizpet.Petfelizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfelizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
